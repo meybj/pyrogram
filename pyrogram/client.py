e978b2506aff3d4e73a8fa0e3ae2869a33fd1fbe
KurimuzonAkuma <arturgayauskas@gmail.com>
2024-03-10 20:43:00 +0300

    Fix recovering gaps in case of private channel

--- pyrogram/client.py
+++ pyrogram/client.py
@@ -592,7 +592,7 @@ class Client(Methods):
                 if pts:
                     await self.storage.update_state(
                         (
-                            utils.get_channel_id(channel_id) if channel_id else self.me.id,
+                            utils.get_channel_id(channel_id) if channel_id else 0,
                             pts,
                             None,
                             updates.date,
@@ -632,7 +632,7 @@ class Client(Methods):
         elif isinstance(updates, (raw.types.UpdateShortMessage, raw.types.UpdateShortChatMessage)):
             await self.storage.update_state(
                 (
-                    self.me.id,
+                    0,
                     updates.pts,
                     None,
                     updates.date,
